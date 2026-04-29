c3fbd8604439e24007910f04ee79a124730f5863
Aleksandrs Stukalovs <stukalovsaleksandrs@gmail.com>
2026-04-29 12:16:29 +0200

    W09: Made mixer's lifetime be managed by a unique_ptr

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -12,40 +12,49 @@
 
 #include "SDL3_mixer/SDL_mixer.h"
 
-static MIX_Mixer *mixer;
-static MIX_Track *track;
-static MIX_Audio *audio;
-void PlaySound()
-{
-    auto& soundService{ Engine::SoundServiceLocator::GetSoundService() };
-    soundService.PlaySound(1);
-
-    Engine::Utils::Check(MIX_Init(), "MIX_Init failed");
-
-    mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
-    Engine::Utils::Check(mixer,"Mixer creation failed");
-
-    std::string const file{ "Resources/GameStart.mp3" };
-    audio = MIX_LoadAudio(mixer, file.data(), true);
-    Engine::Utils::Check(audio, std::format("Filed to load {}", file));
-
-    track = MIX_CreateTrack(mixer);
-    Engine::Utils::Check(track,"Failed to create track");
-
-    MIX_SetTrackAudio(track, audio);
-    MIX_PlayTrack(track, 0);
-
-}
-
 int main(int, char*[]) {
-
     Game::Application game{};
-    PlaySound();
-    game.Run();
+    {
+        auto mixerDeleter{ [](MIX_Mixer* pMixer)
+        {
+            MIX_DestroyMixer(pMixer);
+        } };
+        std::unique_ptr<MIX_Mixer, decltype(mixerDeleter)> pMixer{ nullptr, mixerDeleter};
+        // MIX_Mixer *pMixer;
+        MIX_Audio *audio;
+        MIX_Track *track;
 
-    MIX_DestroyTrack(track);
-    MIX_DestroyAudio(audio);
-    MIX_DestroyMixer(mixer);
+        auto& soundService{ Engine::SoundServiceLocator::GetSoundService() };
+        soundService.PlaySound(1);
+
+        Engine::Utils::Check(MIX_Init(), "MIX_Init failed");
+
+        pMixer.reset(MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL));
+        // pMixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+        Engine::Utils::Check(pMixer.get(),"Mixer creation failed");
+        // Engine::Utils::Check(pMixer,"Mixer creation failed");
+
+        std::string const file{ "Resources/GameStart.mp3" };
+        audio = MIX_LoadAudio(pMixer.get(), file.data(), true);
+        // audio = MIX_LoadAudio(pMixer, file.data(), true);
+        Engine::Utils::Check(audio, std::format("Filed to load {}", file));
+
+        track = MIX_CreateTrack(pMixer.get());
+        // track = MIX_CreateTrack(pMixer);
+        Engine::Utils::Check(track,"Failed to create track");
+
+        MIX_SetTrackAudio(track, audio);
+        MIX_PlayTrack(track, 0);
+
+        game.Run();
+
+        // MIX_StopTrack(track, 0);
+        MIX_DestroyAudio(audio);
+        // Will get destroyed automatically together with the mixer
+        MIX_DestroyTrack(track);
+        // pMixer.reset();
+        // MIX_DestroyMixer(pMixer);
+    }
     MIX_Quit();
     return SDL_APP_SUCCESS;
 }
